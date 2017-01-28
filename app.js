e6d39cebc4f91b1bef93b9f649efab1c7d5013fb
Chris Cooper <cjcooper252@gmail.com>
2017-01-28 13:23:18 +0000

    mocha and chai simple tests

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+var cache = {};
+
+app.get('/', function(req, res) {
+  res.send("Hello world");
+});
+
+app.listen(4000, function() {
+  console.log("Listening on 4000");
+});
+
+app.post('/set', function(req, res) {
+  var query = req.query
+  Object.keys(query).forEach(function(key) {
+    cache[key] = query[key];
+  });
+  res.status(200).end();
+});
+
+app.get('/get', function(req, res) {
+  res.send(cache[req.query.key]);
+})
+
+module.exports = app;
